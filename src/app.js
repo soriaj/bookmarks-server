0959066cbc9991f21f331612cb183e19eca464be
Javier Soria <javier.soria@gmail.com>
2019-09-16 23:09:52 -0700

    add bookmarks-router template

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const bookmarksRouter = require('./bookmarks/booksmarks-router')
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -15,9 +16,22 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use(function validateBearerToken(req, res, next) {
+   const apiToken = process.env.API_TOKEN
+   const authToken = req.get('Authorization')
+
+   if(!authToken || authToken.split(' ')[1] !== apiToken){
+      logger.error(`Unauthorized request to path: ${req.path}`);
+      return res.status(401).json({ error: 'Unauthorized request' })
+   }
+   next()
+})
+
 app.get('/', (req, res) => {
    res.send('Hello, World!')
 })
+
+app.get(bookmarksRouter)
 
 app.use(function errorHandler(error, req, res, next) {
    let response
