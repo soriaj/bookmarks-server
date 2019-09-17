275f4cbbcd8267796cb2fbe670e1e88a4a49aa17
Javier Soria <javier.soria@gmail.com>
2019-09-16 23:59:31 -0700

    add delete by bookmark id

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -63,16 +63,18 @@ bookmarksRouter
       }
       res.json(bookmark)
    })
-   // .delete((req, res) => {
-   //    const { id } = req.params
-   //    const bookmarkIndex = bookmarks.findIndex(book => book.id == id);
+   .delete((req, res) => {
+      const { id } = req.params
+      const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id == id);
 
-   //    if(bookmarkIndex === -1){
-   //       logger.error(`Bookmark with id ${id} not found`);
-   //       return res.status(404).send('Not Found');
-   //    }
+      if(bookmarkIndex === -1){
+         logger.error(`Bookmark with id ${id} not found`);
+         return res.status(404).send('Bookmark Not Found');
+      }
 
-
+      bookmarks.splice(bookmarkIndex, 1);
+      logger.info(`Bookmark with id ${id} deleted.`);
+      res.status(204).end();
    })
 
 module.exports = bookmarksRouter
