e6da9a05bc48715139d70f08329be31d7cf68c29
Javier Soria <javier.soria@gmail.com>
2019-09-21 13:30:02 -0700

    commit before db brach

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ const bookmarks = [
     id: 0,
     title: 'Google',
     url: 'http://www.google.com',
-    rating: 3,
+    rating: 3,  
     description: 'Internet-related services and products.'
   },
   {
