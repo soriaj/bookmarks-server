7986b902fcc2bbe12147bbdbb2e526b3f3db8c98
Javier Soria <javier.soria@gmail.com>
2019-10-02 06:20:50 -0700

    4th rebuild

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL,
+  "host": process.env.MIGRATION_DB_HOST,
+  "port": process.env.MIGRATION_DB_PORT,
+  "database": process.env.MIGRATION_DB_NAME,
+  "username": process.env.MIGRATION_DB_USER,
+  "password": process.env.MIGRATION_DB_PASS,
   "ssl": !!process.env.SSL,
 }
